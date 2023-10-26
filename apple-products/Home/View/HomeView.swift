b5d5d78bc6c7faedb45c1d424b62a920b4c14584
Guilherme Viana <guibviana@icloud.com>
2023-10-26 14:25:51 +0100

    view elements created

--- apple-products/Home/View/HomeView.swift
+++ apple-products/Home/View/HomeView.swift
@@ -9,5 +9,46 @@ import UIKit
 
 class HomeView: UIView {
 
-
+    lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    lazy var sectionNameLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }
+    
+    lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        return searchBar
+    }
+    
+    lazy var filterCollectionView: UICollectionView = {
+        let collectionView = UICollectionView()
+        return collectionView
+    }
+    
+    lazy var productsTableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addElements()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addElements() {
+        addSubview(logoImageView)
+        addSubview(sectionNameLabel)
+        addSubview(searchBar)
+        addSubview(filterCollectionView)
+        addSubview(productsTableView)
+        
+    }
 }
