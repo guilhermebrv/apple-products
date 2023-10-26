b6c43921d628fba96a419f6620f3c1e31e88abc8
Guilherme Viana <guibviana@icloud.com>
2023-10-26 10:31:01 +0100

    created home file structure

---
+++ apple-products/Home/View Model/HomeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModel.swift
+//  apple-products
+//
+//  Created by Guilherme Viana on 26/10/2023.
+//
+
+import UIKit
+
+class HomeViewModel {
+
+}
---
+++ apple-products/Home/View/HomeView.swift
@@ -0,0 +1,13 @@
+//
+//  HomeView.swift
+//  apple-products
+//
+//  Created by Guilherme Viana on 26/10/2023.
+//
+
+import UIKit
+
+class HomeView: UIView {
+
+
+}
