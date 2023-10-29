8a492a89dc8fd48098b38df6d5f747cbb6f39fbb
Guilherme Viana <guibviana@icloud.com>
2023-10-29 12:39:10 +0000

    created configuration for collection view cell, need to setup cell later

--- apple-products/Home/CollectionView/Cell/FilterCollectionViewCell.swift
+++ apple-products/Home/CollectionView/Cell/FilterCollectionViewCell.swift
@@ -9,4 +9,22 @@ import UIKit
 
 class FilterCollectionViewCell: UICollectionViewCell {
     
+    static let identifier: String = String(describing: FilterCollectionViewCell.self)
+    private var screen: FilterCollectionViewCellView = FilterCollectionViewCellView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configScreen()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configScreen() {
+        contentView.addSubview(screen)
+        screen.pin(to: contentView)
+    }
+    
+//  public func setupCell()
 }
